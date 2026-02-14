71d589e5a72859487662f8100dd0b25001e31daf
Naveen-8247 <dyavarnaveen8247@#gmail.com>
2026-02-14 23:05:19 +0530

    Fixed service worker registration for PWA

--- src/index.js
+++ src/index.js
@@ -12,8 +12,25 @@ root.render(
 );
 
 reportWebVitals();
+
+
+
+
+
+
+
+
+
+// SERVICE WORKER REGISTRATION (for PWA Builder)
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js");
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(reg => {
+        console.log("Service Worker registered", reg);
+      })
+      .catch(err => {
+        console.log("Service Worker registration failed", err);
+      });
   });
 }
